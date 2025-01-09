2e1b977aac9c0e1ccee7d82e4534947a10a9ea06
Vitaliy <qw929496@gmail.com>
2025-01-10 00:21:25 +0200

    2

--- Dz10.1/Program.cs
+++ Dz10.1/Program.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace Dz10._1
 {
-    public delegate void AccountEdit(string name, string password);
+    public delegate void account(string message);
     public class Account
     {
-        public event AccountEdit account;
+        public event account accountShow;
         public string Name {  get; private set; }
         public string Password {  get; private set; }
-        public Account(string name, string password)
+        public int Age {  get; private set; }
+        public int H {  get; private set; }
+        public int M {  get; private set; }
+        public Account(string name, string password, int age, int h, int m)
         {
             Name = name;
             Password = password;
+            Age = age;
+            H = h;
+            M = m;
         }
         public bool LoginUser(string name, string password)
         {
-            if (name == Name || password == Password)
+            if (name == Name && password == Password)
             {
                 return true;
             }
@@ -25,28 +32,34 @@ namespace Dz10._1
         {
             ConsoleKeyInfo key;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            do
+            key = Console.ReadKey();
+            if (key.Key == ConsoleKey.I)
             {
-                key = Console.ReadKey();
-                if (key.Key == ConsoleKey.I)
-                {
-
-                }
+                Console.WriteLine();
+                accountShow?.Invoke($"Name: {Name}\nPassword: {Password}\nAge: {Age}\nHeight: {H}\nWeight: {M}");
             }
-            while (key.Key != ConsoleKey.Escape);
         }
     }
     public class Program
     {
         static void Main(string[] args)
         {
-            if (true)
+            Account account1 = new Account("Vitaliy", "123qw", 15, 183, 86);
+            account1.accountShow += (message) =>
             {
-                
+                Console.WriteLine(message);
+            }; 
+            Console.Write("Name: ");
+            string Name = Console.ReadLine();
+            Console.Write("Password: ");
+            string Password = Console.ReadLine();
+            if (account1.LoginUser("Vitaliy", "123qw"))
+            {
+                account1.ShowInfo();
             }
             else
             {
-
+                Console.WriteLine("Error");
             }
         }
     }
---
+++ Dz10.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dz10._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
